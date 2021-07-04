a2b4a96443fd88e24f8ccae5fbb6a07d02affda8
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-07-04 16:31:28 -0600

    Added background image overlay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Quote from './components/Quote';
+import Time from './components/Time';
+import MoreInfo from './components/MoreInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app-container">
+      <Quote />
+      <section>
+        <Time />
+        <MoreInfo />
+      </section>
+    </main>
   );
 }
 
