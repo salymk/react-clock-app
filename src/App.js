b32682971121434f326450bf170d0d5751ade243
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-07-04 18:58:29 -0600

    Added button and button styles

--- src/App.js
+++ src/App.js
@@ -42,9 +42,17 @@ function App() {
             <h2>IN LONDON, UK</h2>
           </div>
         </div>
-        <button className="refresh-btn" type="button">
-          More
-        </button>
+        <div>
+          <button className="more-info-btn" type="button">
+            <span> More </span>
+            <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fillRule="evenodd">
+                <circle fill="#303030" cx="20" cy="20" r="20" />
+                <path stroke="#FFF" strokeWidth="2" d="M14 23l6-6 6 6" />
+              </g>
+            </svg>
+          </button>
+        </div>
       </div>
     </main>
   );
