88947f0603c100b52893defbbc2132ead9a435d1
Salym Akhmedov <khokumdarakhmedov@gmail.com>
2021-07-23 15:57:24 -0600

    Removed div container on button

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   }
 
   const time = DateTime.fromSeconds(data.unixtime);
-  const currentHour = time.hour;
+  const currentHour = 6;
   const dayTime = currentHour > 5 && currentHour < 18;
 
   return (
@@ -33,26 +33,24 @@ function App() {
         <Quote toggle={toggle} />
         <div className="time-container">
           <Time />
-          <div>
-            <button
-              onClick={() => setToggle(!toggle)}
-              className="more-info-btn"
-              type="button"
+          <button
+            onClick={() => setToggle(!toggle)}
+            className="more-info-btn"
+            type="button"
+          >
+            <span> {toggle ? 'Less' : 'More'} </span>
+            <svg
+              className={`arrow-svg ${toggle ? 'arrow-svg-up' : ''}`}
+              width="40"
+              height="40"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <span> {toggle ? 'Less' : 'More'} </span>
-              <svg
-                className={`arrow-svg ${toggle ? 'arrow-svg-up' : ''}`}
-                width="40"
-                height="40"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g fill="none" fillRule="evenodd">
-                  <circle fill="#303030" cx="20" cy="20" r="20" />
-                  <path stroke="#FFF" strokeWidth="2" d="M14 23l6-6 6 6" />
-                </g>
-              </svg>
-            </button>
-          </div>
+              <g fill="none" fillRule="evenodd">
+                <circle fill="#303030" cx="20" cy="20" r="20" />
+                <path stroke="#FFF" strokeWidth="2" d="M14 23l6-6 6 6" />
+              </g>
+            </svg>
+          </button>
         </div>
         <MoreInfo toggle={toggle} bgDayOrNight={dayTime} />
       </main>
